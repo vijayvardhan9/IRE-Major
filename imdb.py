c5519b25ee982612be3d5a98353925e8d193d887
“vijayvardhan9” <vijayvardhan.a@research.iiit.ac.in>
2020-11-18 00:36:37 +0530

    Fixed bugs

--- imdb.py
+++
@@ -1,7 +0,0 @@
-import csv
-import sys
-with open("movies.txt", "r") as f:
-    csvreader = csv.reader(f, delimiter = '\t')
-    for line in csvreader:
-        if line[2].lower() == sys.argv[1]:
-            print(line[0])
